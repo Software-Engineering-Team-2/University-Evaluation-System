8dd9467e9e48a34b25c15ce940da75b743b70f05
Anand Kumar <ak05173@st.habib.edu.pk>
2021-04-18 17:40:16 +0500

    remove secret key

--- server/src/drf_api/settings.py
+++ server/src/drf_api/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '42rh@_1my$e%m-tqkz8$16bnxff_i6uo33*d7xt_2lu5=04b6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
